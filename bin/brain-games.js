ec7e3ded3c5fbfcac47613b7e179e4cc7c79fe61
AlexPasky <paskyab@gmail.com>
2020-05-28 22:13:03 +0300

    change brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,4 @@ import userName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const userName1 = userName();
-console.log('Hi ' + userName1 + ' !');
+console.log(`Hi ${userName1} !`);
